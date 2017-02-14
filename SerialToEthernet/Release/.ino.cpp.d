47d49bbf1e49c365633f7fc4765d29acb9cd555c
Luca Remigio <Luca Remigio@TH-13DEV99.taiprora1.it>
2017-02-14 11:42:57 +0100

    initial commit

---
+++ SerialToEthernet/Release/.ino.cpp.d
@@ -0,0 +1,76 @@
+.ino.cpp.o: ../.ino.cpp \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\mega/pins_arduino.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\libraries\SPI\src/SPI.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/Ethernet.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/IPAddress.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/EthernetClient.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Client.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/IPAddress.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/EthernetServer.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Server.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/Dhcp.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/EthernetUdp.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Udp.h \
+ C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ ../SerialToEthernet.ino
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\variants\mega/pins_arduino.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\libraries\SPI\src/SPI.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/Ethernet.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/IPAddress.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/EthernetClient.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Client.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/IPAddress.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/EthernetServer.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Server.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/Dhcp.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\libraries\Ethernet\1.1.2\src/EthernetUdp.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Udp.h:
+
+C:\Users\Progetti\Eclipse\openhab2-master\eclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h:
+
+../SerialToEthernet.ino:
